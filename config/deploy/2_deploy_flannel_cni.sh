dc295b454d2e7d1ac69726be93b9f9d65bc96d05
root <root@ip-10-0-6-54.ap-northeast-2.compute.internal>
2023-06-18 06:02:59 +0000

    123

--- config/deploy/2_deploy_flannel_cni.sh
+++ config/deploy/2_deploy_flannel_cni.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-sudo kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
