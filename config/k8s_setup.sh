d198a23971195d681c492ea0545a02fe02eed0c7
root <root@ip-10-0-13-69.ap-northeast-2.compute.internal>
2023-07-26 08:55:45 +0000

    Update k8s_install script

--- config/k8s_setup.sh
+++ config/k8s_setup.sh
@@ -1,9 +1,12 @@
 #/bin/bash
 sudo swapoff -a && sed -i '/swap/s/&/#/' /etc/fstab
-sudo systemctl stop firewalld && sudo systemctl disable firewalld
-sudo curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-sudo mkdir /etc/docker
+sudo ufw disable
+sudo apt-get update
+# CRI: Docker Containerd Package Configuration
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl bridge-utils gnupg lsb-release docker-ce docker-ce-cli containerd.io bash-completion
 sudo cat <<EOF | sudo tee /etc/docker/daemon.json
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
@@ -18,14 +21,12 @@ sudo systemctl enable docker
 sudo systemctl start docker
 sudo sed -i -e '/disabled_plugins/ s/^/#/' /etc/containerd/config.toml
 sudo systemctl restart containerd
-sudo cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
-sudo yum update
-sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
-sudo systemctl daemon-reload
+# Kubernetes Package Configuration
+sudo mkdir -p /etc/apt/keyrings
+sudo curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+sudo echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt install kubeadm=1.27.4-00 kubelet=1.27.4-00 kubectl=1.27.4-00 -y
+sudo apt-mark hold kubeadm kubelet kubectl docker
+sudo systemctl enable kubelet
 sudo systemctl restart kubelet
