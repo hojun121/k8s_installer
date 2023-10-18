24c3ef0fb20c6fa3fc60968441b97793b1b233bb
jaehopark <107022839+hojun121@users.noreply.github.com>
2023-10-19 00:17:06 +0900

    Update 1_kubectl_setup.sh

--- config/deploy/1_kubectl_setup.sh
+++ config/deploy/1_kubectl_setup.sh
@@ -3,7 +3,7 @@ sudo mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 echo 'source /etc/profile.d/bash_completion.sh' >>~/.bashrc
-echo 'source <(kubectl completion bash)' >>~.bashrc
+echo 'source <(kubectl completion bash)' >>~/.bashrc
 echo 'alias k=kubectl' >>~/.bashrc
 echo 'complete -o default -F __start_kubectl k' >>~/.bashrc
 source ~/.bashrc
