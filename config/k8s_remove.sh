2cd1ee12615bdea992ffd4fd168d9da8d904df93
jaehopark <107022839+hojun121@users.noreply.github.com>
2024-01-17 18:55:59 +0900

    Update k8s_remove.sh

--- config/k8s_remove.sh
+++ config/k8s_remove.sh
@@ -1,17 +1,10 @@
 #/bin/bash
 sudo kubeadm reset --force
 sudo systemctl stop kubelet
-sudo systemctl stop docker
-sudo apt-mark unhold kubeadm kubelet kubectl docker
-sudo apt-get purge kubeadm kubelet kubectl docker.io --auto-remove -y
+sudo apt-mark unhold kubeadm kubelet kubectl
+sudo apt-get purge kubeadm kubelet kubectl --auto-remove -y
 sudo rm -rf /var/lib/cni/
 sudo rm -rf /var/lib/kubelet/*
 sudo rm -rf /etc/cni/
 sudo rm -rf /etc/kubernetes
-sudo rm -rf /etc/docker
 sudo rm -rf $HOME/.kube
-sudo ifconfig cni0 down
-sudo ifconfig flannel.1 down
-sudo ifconfig docker0 down
-sudo brctl delbr cni0
-sudo brctl delbr docker0
