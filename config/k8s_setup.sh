7ca484068a3645ee00477c74cb11d4de540668a9
jaehopark <107022839+hojun121@users.noreply.github.com>
2024-01-17 18:55:02 +0900

    Update k8s_setup.sh

--- config/k8s_setup.sh
+++ config/k8s_setup.sh
@@ -1,32 +1,31 @@
-#/bin/bash
-sudo swapoff -a && sed -i '/swap/s/&/#/' /etc/fstab
+sudo echo "sudo apt-get update -y
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo swapoff -a
+sudo sed -i '/swap/s/&/#/' /etc/fstab
 sudo ufw disable
-sudo apt-get update
-# CRI: Docker Containerd Package Configuration
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl bridge-utils gnupg lsb-release docker-ce docker-ce-cli containerd.io bash-completion
-sudo cat <<EOF | sudo tee /etc/docker/daemon.json
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2"
-}
-EOF
-sudo systemctl enable docker
-sudo systemctl start docker
-sudo sed -i -e '/disabled_plugins/ s/^/#/' /etc/containerd/config.toml
-sudo systemctl restart containerd
-# Kubernetes Package Configuration
 sudo mkdir -p /etc/apt/keyrings
-sudo curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
-sudo echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt install kubeadm=1.27.4-00 kubelet=1.27.4-00 kubectl=1.27.4-00 -y
-sudo apt-mark hold kubeadm kubelet kubectl docker
-sudo systemctl enable kubelet
-sudo systemctl restart kubelet
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg  https://dl.k8s.io/apt/doc/apt-key.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update -y
+sudo apt-get install -y kubelet=1.28.2-00 kubeadm=1.28.2-00 kubectl=1.28.2-00
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo cat <<EOF | sudo tee /etc/modules-load.d/crio.conf
+overlay
+br_netfilter
+EOF
+sudo modprobe overlay
+sudo modprobe br_netfilter
+sudo cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+sudo sysctl --system
+sudo echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_20.04/ /" | sudo tee -a /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+sudo echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/1.26/xUbuntu_20.04/ /" | sudo tee -a /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:1.26.list
+sudo curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:1.26/xUbuntu_20.04/Release.key | sudo apt-key add -
+sudo curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_20.04/Release.key | sudo apt-key add -
+sudo apt-get update -y
+sudo apt-get install cri-o cri-o-runc -y
+sudo systemctl daemon-reload
+sudo systemctl enable crio --now" | sudo tee -a /home/install.sh && sudo chmod +x /home/install.sh && sudo sh /home/install.sh
